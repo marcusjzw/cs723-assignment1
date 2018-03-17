1862671b1e5cd00998c55553731d4c3ed7e2084f
Serena Toon <stoo718@aucklanduni.ac.nz>
2018-03-17 13:21:59 +1300

    lab stuff

---
+++ software/freertos_test/obj/default/H/Documents/cs723/software/freertos_test/freertos/port.d
@@ -0,0 +1,74 @@
+obj/default//H/Documents/cs723/software/freertos_test/freertos/port.o:  \
+ H:/Documents/cs723/software/freertos_test/freertos/port.c \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/sys/alt_irq.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/nios2.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/linker.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/priv/alt_legacy_irq.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/nios2.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/sys/alt_irq.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//drivers/inc/altera_avalon_timer_regs.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/io.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/priv/alt_irq_table.h \
+  H:/Documents/cs723/software/freertos_test/freertos/FreeRTOS.h \
+  H:/Documents/cs723/software/freertos_test/freertos/FreeRTOSConfig.h \
+  //uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h \
+  H:/Documents/cs723/software/freertos_test/freertos/projdefs.h \
+  H:/Documents/cs723/software/freertos_test/freertos/portable.h \
+  H:/Documents/cs723/software/freertos_test/freertos/deprecated_definitions.h \
+  H:/Documents/cs723/software/freertos_test/freertos/portmacro.h \
+  H:/Documents/cs723/software/freertos_test/freertos/mpu_wrappers.h \
+  H:/Documents/cs723/software/freertos_test/freertos/task.h \
+  H:/Documents/cs723/software/freertos_test/freertos/list.h
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/sys/alt_irq.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/nios2.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/linker.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/nios2.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/sys/alt_irq.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/io.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/alt_types.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp//HAL/inc/priv/alt_irq_table.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/FreeRTOS.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/FreeRTOSConfig.h:
+
+//uoa.auckland.ac.nz/engdfs/Home/stoo718/Documents/cs723/software/freertos_test_bsp/system.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/projdefs.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/portable.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/deprecated_definitions.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/portmacro.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/mpu_wrappers.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/task.h:
+
+H:/Documents/cs723/software/freertos_test/freertos/list.h:
---
+++ software/freertos_test/obj/default/H/Documents/cs723/software/freertos_test/freertos/port_asm.d
@@ -0,0 +1,3 @@
+ \
+ obj/default//H/Documents/cs723/software/freertos_test/freertos/port_asm.o:  \
+ H:/Documents/cs723/software/freertos_test/freertos/port_asm.S
