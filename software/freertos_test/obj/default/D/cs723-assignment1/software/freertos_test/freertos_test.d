ce53e15d72d3bc8af509f7bbb602eb2f7388753a
Serena Toon <stoo718@UoA.auckland.ac.nz>
2018-04-06 19:24:56 +1200

    working freq relay with ignoring useless values + code for vga task

--- software/freertos_test/obj/default/D/cs723-assignment1/software/freertos_test/freertos_test.d
+++ software/freertos_test/obj/default/D/cs723-assignment1/software/freertos_test/freertos_test.d
@@ -25,7 +25,14 @@ obj/default//D/cs723-assignment1/software/freertos_test/freertos_test.o:  \
   D:/cs723-assignment1/software/freertos_test/freertos/queue.h \
   D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_avalon_pio_regs.h \
   D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/io.h \
-  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/alt_types.h
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/alt_types.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/alt_types.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_dev.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_llist.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/priv/alt_dev_llist.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_llist.h \
+  D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h
 
 D:/cs723-assignment1/software/freertos_test/freertos/FreeRTOS.h:
 
@@ -78,3 +85,17 @@ D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_avalon_pio_r
 D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/io.h:
 
 D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/alt_types.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/alt_types.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_dev.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_llist.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//HAL/inc/sys/alt_llist.h:
+
+D:/cs723-assignment1/software/freertos_test_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
